58a9b0845ee48ce853e9a9220256403cf86d5662
Viktoria <vikalex11@mail.ru>
2022-04-14 10:20:53 +0300

    feat: delete unnecessary layout

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -1,21 +1,5 @@
-import { useSelector } from 'react-redux';
 import './mainpage.scss';
 
 export default function MainPage() {
-  const { auth } = useSelector((state) => state);
-
-  return (
-    <div className='main_wrapper'>
-      <h1> Hi, {auth.user.first_name}</h1>
-      <div>
-        <h2>I know, that your surname is {auth.user.last_name}</h2>
-      </div>
-      <div>
-        <h2>Maybe your email is {auth.user.email}?</h2>
-      </div>
-      <div>
-        <h2>Anyway, nice to meet you!</h2>
-      </div>
-    </div>
-  );
+  return <div className='main_wrapper'></div>;
 }
